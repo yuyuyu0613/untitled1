6223dd2fc1b0e7cde61f1ad6be4467002c276bba
ayuyu <ayuyuwoai@qq.com>
2022-09-07 21:36:48 +0800

    third commit

--- src/comaa/add.java
+++ src/comaa/add.java
@@ -6,5 +6,7 @@ public class add {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("held lo");
+        System.out.println("hpo lp");
+
     };
 }
